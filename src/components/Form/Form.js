0197874ddf6af33872d09a95cc268a64ff790a18
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-07-26 17:03:05 -0400

    created handleSubmit fn which dispatches createPost action

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import {useDispatch} from 'react-redux'
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import FileBase from "react-file-base64";
+
+import {createPost } from "../../actions/posts"
 
 import useStyles from "./styles";
 
@@ -15,7 +18,13 @@ const Form = () => {
 
   const classes = useStyles();
 
-  const handleSubmit = () => {};
+  const dispatch = useDispatch()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    dispatch(createPost(postData))
+  };
 
   const clear = () => {};
 
