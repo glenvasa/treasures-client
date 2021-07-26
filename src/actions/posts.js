d44ccdd2e93710171d741c92a24c888462f99159
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-07-26 17:02:23 -0400

    created createPost action

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,18 +1,21 @@
-import * as api from '../api/index'
+import * as api from "../api/index";
 
 // Action Creators
-export const getPosts = () => async(dispatch) => {
+export const getPosts = () => async (dispatch) => {
+  try {
+    const { data } = await api.fetchPosts();
 
-    try {
-        const { data } = await api.fetchPosts()
-        
-        dispatch({ type: 'FETCH_ALL', payload: data})
-    } catch (error) {
-        console.log(error.message)
-    }
+    dispatch({ type: "FETCH_ALL", payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+export const createPost = (post) => async (dispatch) => {
+  try {
+    const { data } = await api.createPost(post);
 
-
-   
-
-    
-}
+    dispatch({ type: "CREATE_POST", payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
