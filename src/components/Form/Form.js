fa76ca5d6199369e13f645ce1525448d52351f62
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-07-29 17:32:26 -0400

    added functionality to Update/Edit a Post

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,32 +1,46 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import FileBase from "react-file-base64";
 
-import { createPost } from "../../actions/posts";
+import { createPost, updatePost } from "../../actions/posts";
 
 import useStyles from "./styles";
 
-const Form = () => {
-  const [postData, setPostData] = useState({
-    creator: "",
-    title: "",
-    message: "",
-    tags: "",
-    selectedFile: "",
-  });
+const Form = ({ currentId, setCurrentId }) => {
+  const [postData, setPostData] = useState({creator: "", title: "", message: "", tags: "", selectedFile: ""});
+
+  // finds the specific post whose data we want to automatically populate in the form for updating
+  const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null)
 
   const classes = useStyles();
 
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    if(post) setPostData(post)
+  }, [post])
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    dispatch(createPost(postData));
+    // if the post data in the form already has an id, then we want the submit handler to update
+    // the post, not create a new one. If no currentId exists, then just create a new post.
+    if (currentId) {
+      dispatch(updatePost(currentId, postData));
+    } else {
+      dispatch(createPost(postData));
+    }
+    
+    clear()
   };
 
-  const clear = () => {};
+  const clear = () => {
+    setCurrentId(null)
+    setPostData({creator: "", title: "", message: "", tags: "", selectedFile: ""});
+
+  };
 
   return (
     // Paper is basically a div w/ a whitish background
@@ -37,7 +51,7 @@ const Form = () => {
         className={`${classes.root} ${classes.form}`}
         onSubmit={handleSubmit}
       >
-        <Typography variant="h6">Share a Treasure</Typography>
+        <Typography variant="h6">{currentId ? 'Editing' : 'Creating'} Your Treasure</Typography>
         <TextField
           name="creator"
           variant="outlined"
