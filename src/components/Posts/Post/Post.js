309000c0a4971ec8e8b40917889144ce547870a4
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-07-29 17:33:31 -0400

    added setCurrentId when a post's edit button is clicked

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -14,7 +14,7 @@ import moment from "moment";
 
 import useStyles from "./styles";
 
-const Post = ({ post }) => {
+const Post = ({ post, setCurrentId }) => {
   const classes = useStyles();
   return (
     <Card className={classes.card}>
@@ -30,7 +30,11 @@ const Post = ({ post }) => {
         </Typography>
       </div>
       <div className={classes.overlay2}>
-        <Button style={{ color: "white" }} size="small" onClick={() => {}}>
+        {/* post edit button */}
+        <Button 
+        style={{ color: "white" }} 
+        size="small" 
+        onClick={() => setCurrentId(post._id)}>
           <MoreHorizIcon fontSize="default" />
         </Button>
       </div>
@@ -39,8 +43,10 @@ const Post = ({ post }) => {
           {post.tags.map((tag) => `#${tag} `)}
         </Typography>
       </div>
+      <Typography className={classes.title} variant="h5" gutterBottom>
+          {post.title}  </Typography>
       <CardContent>
-        <Typography className={classes.title} variant="h5" gutterBottom>
+        <Typography variant="h5" gutterBottom>
           {post.message}
         </Typography>
       </CardContent>
